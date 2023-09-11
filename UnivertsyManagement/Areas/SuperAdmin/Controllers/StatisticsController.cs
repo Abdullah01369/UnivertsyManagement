0342a4673e897c61287f2798db36cca1bca709da
Abdullah01369 <127797036+Abdullah01369@users.noreply.github.com>
2023-09-11 23:29:16 +0300

    Added Statistic

---
+++ UnivertsyManagement/Areas/SuperAdmin/Controllers/StatisticsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnivertsyManagement.Areas.SuperAdmin.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: SuperAdmin/Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StatisticByAcademicianTitle()
+        {
+            return PartialView("PartialStatisticByAcademicianTitle");
+        }
+        public ActionResult StatisticByAcademicianPublication()
+        {
+            return PartialView("PartialStatisticPublicationByAcademician");
+        }
+    }
+}
